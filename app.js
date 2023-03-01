1f0d2b7773eda376f924b51f830d682fb7b28e1c
shashwat <shashwat0313@yahoo.co.in>
2023-03-01 13:32:45 +0530

    issue: reload causes checked items to become unchecked

--- app.js
+++ app.js
@@ -2,34 +2,47 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const { redirect } = require('express/lib/response');
 const app = express();
+const date = require(__dirname + '/date.js');
+console.log(date.getday());
+console.log(date.getdate());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 let new_task = "";
 let TaskList = [];
+let worklist = [];
 
-var today = new Date();
 app.get('/', (req, res) => {
-    res.render('list', { dayvalue: today.toLocaleDateString('en-IN', { weekday: 'long' }), taskArray:TaskList});
+    res.render('list', { dayvalue: date.getdate(),taskArray:TaskList});
 });
 
 app.post('/', function (req, res) {
-    if(req.body.task == ''){
-        res.redirect('/');
-        return;
-    }
-    if(TaskList.length == 15){
-        TaskList = [];
-        console.log("array was full so it has been emptiedl.");
+    console.log(req.body);
+    let list = req.body.list;
+    let item = req.body.task;
+    console.log('received list=' + list);
+    if(list == "Work"){
+        console.log('received in work...list=' + list)
+        worklist.push(item);
+        res.redirect('/work');
+        res.send();
     }
     else{
-        TaskList.push(req.body.task);
+        console.log('received in regular...list=' + list);
+        TaskList.push(item);
+        res.redirect('/');
+        res.send();
     }
-    // console.log(TaskList[TaskList.length-1]);
-    // console.log("received", JSON.stringify(req.body.task));
-    res.render('list', { dayvalue: today.toLocaleDateString('en-IN', { weekday: 'long' }), taskArray:TaskList});
-    res.send();
+})
+
+app.get('/work', (req, res) => {
+    res.render('list', { dayvalue: "Work List", taskArray:worklist});
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
 })
 
 app.listen(3000, () => {
